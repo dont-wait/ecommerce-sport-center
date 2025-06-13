e36724c8514e486f614aa86eeda93e9228c08ed4
dontwait <nguyentansang15042005@gmail.com>
2025-06-13 16:02:18 +0700

    feat: init dependency for project

---
+++ server/ecommerce-sports-center/src/test/java/com/dontwait/ecommerce_sports_center/EcommerceSportsCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dontwait.ecommerce_sports_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSportsCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
