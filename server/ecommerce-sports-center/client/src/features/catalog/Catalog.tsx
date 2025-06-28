c1d31a89f783dc47a71003bd544fdd1f2f19d6c5
dontwait <nguyentansang15042005@gmail.com>
2025-06-28 14:19:55 +0700

    update: add public folder

--- server/ecommerce-sports-center/client/src/features/catalog/Catalog.tsx
+++ server/ecommerce-sports-center/client/src/features/catalog/Catalog.tsx
@@ -2,12 +2,13 @@ import { useState, useEffect } from "react";
 import { apiClient } from "../../api/axios";
 import type { Product } from "../../app/models/Product";
 import { useToast } from "../../hooks/useToast";
+import ProductList from "./ProductList";
 
 export default function Catalog() {
     const { showToast } = useToast();
     //Define a state variable products, using useState
     const [products, setProducts] = useState<Product[]>([]);
-    
+
     useEffect(() => {
         const fetchProducts = async () => {
             try {
@@ -24,17 +25,8 @@ export default function Catalog() {
     }, []);
 
     return (
-        <div>
-            {
-                products.map((product) => (
-                    <div key={product.id} className="product">
-                        <p>Name: {product.name}</p>
-                        <p>Description: {product.description}</p>
-                        <p>Price: ${product.price}</p>
-                        <p>Brand: {product.productBrand}</p>
-                        <p>Type: {product.productType}</p>
-                    </div>
-                ))}
-        </div>
+        <>
+            <ProductList products={products} />
+        </>
     );
 }
